24df8a866dcd76e6d8b6e60260b1a0d6f897287b
Dennis Sänger <mail@dennis.io>
2015-07-24 13:09:32 +0200

    chore(doc): add script to build demo in gh-pages

---
+++ bin/gh-pages.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+	git checkout gh-pages
+	git reset --hard master
+  npm install
+	git add -f node_modules
+	git commit -m "doc(demo): add node_modules for demo"
+	git push -f
+	git checkout master
